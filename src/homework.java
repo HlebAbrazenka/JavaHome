21642091b01afd8217d1c3af15347d5ad3c7f007
HlebAbrazenka <dr.bread92@gmail.com>
2023-01-17 22:06:45 +0100

    Lesson1

---
+++ src/homework.java
@@ -0,0 +1,11 @@
+/**
+ * Java. My first program
+ *
+ * @author HlebAbrazenk
+ * 28.12.2022
+ */
+public class homework {
+    public static void main(String[] args) {
+        System.out.print("HELLO IqT"); //output
+    }
+}
