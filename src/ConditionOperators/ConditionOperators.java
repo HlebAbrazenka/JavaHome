cc56f19b6f2c4c64aa880411c95ce3974c26573f
HlebAbrazenka <dr.bread92@gmail.com>
2023-01-19 21:40:57 +0100

    Lesson1

---
+++ src/ConditionOperators/ConditionOperators.java
@@ -0,0 +1,14 @@
+package ConditionOperators;
+
+public class ConditionOperators {
+    public static void main(String[] args) {
+        int x =-10;
+        if (x < 0) {
+            System.out.println("x = " + x +"and x>0");
+        } else if (x<0) {
+            System.out.println("x = " + x +"and x<0");
+        } else {
+            System.out.println("x = 0 !!!!");
+        }
+    }
+}
