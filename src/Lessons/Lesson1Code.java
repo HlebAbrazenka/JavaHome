3b6ebb45c94aa6937e07e9be94804a9b14a73047
HlebAbrazenka <dr.bread92@gmail.com>
2023-01-21 17:34:22 +0100

    Lesson

---
+++ src/Lessons/Lesson1Code.java
@@ -0,0 +1,10 @@
+package Lessons;
+
+public class Lesson1Code {
+    public static void main(String[] args) {
+        String t1 = "Hello, World!";
+        System.out.println(t1);
+        t1= "Hello!!!";
+        System.out.println(t1);
+    }
+}
