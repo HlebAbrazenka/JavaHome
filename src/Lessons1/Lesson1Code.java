2b99435bb8358d236e1625793b84bb253023473c
HlebAbrazenka <dr.bread92@gmail.com>
2023-01-21 17:35:54 +0100

    Lesson

--- src/Lessons1/Lesson1Code.java
+++ src/Lessons1/Lesson1Code.java
@@ -1,4 +1,4 @@
-package Lessons;
+package Lessons1;
 
 public class Lesson1Code {
     public static void main(String[] args) {
--- src/Lessons1/Main.java
+++ src/Lessons1/Main.java
@@ -1,4 +1,4 @@
-package Lessons;
+package Lessons1;
 
 public class Main {
     public static void main(String[] args){
