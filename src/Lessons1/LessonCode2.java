743f763d16aa9a4936c213f410a80a87793a5bf1
HlebAbrazenka <dr.bread92@gmail.com>
2023-01-21 18:09:18 +0100

    Lesson1f

---
+++ src/Lessons1/LessonCode2.java
@@ -0,0 +1,24 @@
+package Lessons1;
+
+public class LessonCode2 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 2;
+        int c = 4;
+
+        double result = a / (double) b;
+
+        int result2 = b*c;
+
+        System.out.println(result);
+
+        String t1 = "Hello, World!";
+
+        System.out.println(t1);
+
+        t1= "Hello!";
+
+        System.out.println(t1);
+
+    }
+}
